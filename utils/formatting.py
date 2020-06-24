0a49380312075fa591a9a4b37e7be59b9785fbec
ALan <alan_biju@outlook.com>
2020-06-24 02:01:06 -0400

    Improved code.

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,8 +1,7 @@
 def casify(text, case_type='normal'):
     if case_type == 'normal':
-        if '-' in text or '_' in text:
-            text = text.replace('-', ' ')
-            text = text.replace('_', ' ')
+        if '-' in text: text = text.replace('-', ' ')
+        if '_' in text: text = text.replace('_', ' ')
         tokens = text.split(' ')
         return ' '.join(i.strip().capitalize() for i in tokens if i.strip())
     else:
