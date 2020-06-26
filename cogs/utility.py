f7f69514c7945aab88eae1e404a4e9eb0b863890
ALan <alan_biju@outlook.com>
2020-06-26 07:42:22 -0400

    Added wikipedia command.

--- cogs/utility.py
+++ cogs/utility.py
@@ -7,10 +7,15 @@ from discord.ext import commands
 import time
 # Asynchronous Requests.
 import aiohttp
+# Randomization.
+import random
+# Wikiepdia Library.
+import wikipedia
 # JSON Parser.
 from utils import default
 # DateTime Parser.
 from datetime import datetime
+
 
 class UtilityCog(commands.Cog):
     """Utility Commands."""
@@ -97,6 +102,64 @@ class UtilityCog(commands.Cog):
         embed.timestamp = datetime.utcnow()
         await ctx.send(embed = embed)
     
+    @commands.command(usage = '<search/summary/random> [query]', aliases = ["wikipedia"])
+    async def wiki(self, ctx, param: str, *, stuff = None):
+        """Searches for articles on Wikipedia."""
+
+        param = param.lower().strip()
+
+        if param == 'search':
+            if stuff is None:
+                raise commands.BadArgument(message = 'missing query')
+            else:
+                results = wikipedia.search(stuff)
+                new_results = [f"{count}. {result}" for count, result in enumerate(results)]
+                await ctx.send(f"I searched for {stuff} and got the following results.")
+                return await ctx.send("\n".join(new_results))
+
+        elif param == 'summary':
+
+            if stuff is None:
+                raise commands.BadArgument('missing query')
+            else:
+                msg = await ctx.send("🌐 **Searching...**")
+                try:
+                    summary = wikipedia.summary(stuff, sentences = 5, auto_suggest = False)
+
+                    embed = discord.Embed(title = stuff.capitalize(), description = summary, color = self.colors.primary)
+                    embed.set_footer(text = "Powered by wikipedia.org")
+                    await ctx.send(embed = embed)
+                except Exception as ex:
+                    await ctx.send(f"{self.emojis.cross} **Unknown Exception.** `[ex {ex}]`")
+                    await ctx.send("Please try again later or with a different search query.\nMy developer has been notified of the issue.")
+
+                    # send dm to a random dev
+                finally:
+
+                    await msg.delete()
+
+        elif param.lower() == 'random':
+            msg = await ctx.send("🎲 **Randomizing...**")
+            try:
+                rand = wikipedia.random()
+                print(rand)
+                rand = random.choice(rand)
+                summary = wikipedia.summary(rand, sentences = 5)
+
+                embed = discord.Embed(title = rand, description = summary, color = self.colors.primary)
+                embed.set_footer(text = "Powered by wikipedia.org")
+                await ctx.send(embed = embed)
+            except Exception as ex:
+                await ctx.send(f"{self.emojis.cross} **Unknown Exception.** `[ex {ex}]`")
+                await ctx.send("Please try again later.\nMy developer has been notified of the issue.")
+                #owner = self.bot.get_user(random.choice(list(self.bot.owner_ids)))
+            finally:
+                await msg.delete()
+        
+        else:
+
+            raise commands.BadArgument('missing parameter')
+    
     @commands.command()
     async def invite(self, ctx):
         """Gives you a link to invite me!"""
@@ -104,7 +167,6 @@ class UtilityCog(commands.Cog):
         invite_url = f"https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=2147483095&scope=bot"
         embed = discord.Embed(color = self.colors.secondary, description = f"🔗  You can invite me using __**[this link!]({invite_url})**__")
         await ctx.send(embed = embed)
-        
     
     #@commands.command()
     #async def reverse(self, ctx, *, text = None):
