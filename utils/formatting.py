c3852fe1f70b78f25362789c562352de45f6dee3
Alan Biju <=>
2020-07-27 06:36:21 -0400

    Cooldowns and a lot more.
    Added cooldowns to all commands to prevent spam, added roleinfo, modified existing info commands, corrected base command order in the help menu, fixed text formatting functions.

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,6 +1,6 @@
 def casify(text, case_type='pascal'):
     if case_type == 'pascal':
-        return text.title().replace('_', '').replace('-', '')
+        return text.title().replace('_', ' ').replace('-', ' ')
     elif case_type == 'camel':
         tex = casify(text, case_type = 'pascal')
         if len(text.split(' ')) > 1: return text[0].lower() + tex[1:]
