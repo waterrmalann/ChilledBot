fcd65e2dd7da818482de31c9ed90e86e5dd7b48a
Alan Biju <=>
2020-08-03 02:40:15 -0400

    type hints and much cleaner, better code

--- utils/formatting.py
+++ utils/formatting.py
@@ -1,10 +1,29 @@
-def casify(text, case_type='pascal'):
-    if case_type == 'pascal':
-        return text.title().replace('_', ' ').replace('-', ' ')
-    elif case_type == 'camel':
-        tex = casify(text, case_type = 'pascal')
-        if len(text.split(' ')) > 1: return text[0].lower() + tex[1:]
-        else: return tex
-    else:
-        raise ValueError('Inappropriate argument value. Use a proper case type.')
+def casify(text: str, case_type: str = 'title') -> str:
+    """Format text to specific case types. Accepts regular cased strings, snake case, and kebab case"""
 
+    if case_type == 'title':  # This Is An Example
+        return text.title().replace('_', ' ').replace('-', ' ')
+    elif case_type == 'pascal':  # ThisIsAnExample
+        return text.title().replace('_', '').replace('-', '').replace(' ', '')
+    elif case_type == 'camel':  # thisIsAnExample
+        return text[0].lower() + text.title().replace('_', '').replace('-', '').replace(' ', '')[1:]
+    elif case_type == 'skewer' or case_type == 'kebab':  # this-is-an-example
+        return text.lower().replace('_', '-').replace(' ', '-')
+    elif case_type == 'skewer_upper' or case_type == 'kebab_upper':  # THIS-IS-AN-EXAMPLE
+        return text.upper().replace('_', '-').replace(' ', '-')
+    elif case_type == 'snake':  # this_is_an_example
+        return text.lower().replace(' ', '_').replace('-', '_')
+    elif case_type == 'snake_upper':  # THIS_IS_AN_EXAMPLE
+        return text.upper().replace(' ', '_').replace('-', '_')
+    else:
+        raise ValueError('invalid case type')
+
+def join_words(li: list, oxford_comma: bool = True) -> str:
+	"""Join a list of words by comma with an 'and' for the last item."""
+	
+	if not li:  # Empty list
+		return ''
+	elif len(li) == 1:  # Single item in list
+		return li[0]
+	else:  
+		return f"{', '.join(li[:-1])}{', and' if oxford_comma else ' and'} {li[-1]}"
