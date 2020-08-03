69691f7a7d60907e96ede4a9c5a88e7ee96b67cf
ALan <alan_biju@outlook.com>
2020-08-03 02:54:52 -0400

    uses converters externally now, typo fixes, mute and unmute, role commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,20 +3,8 @@ import discord
 # Command Handler.
 from discord.ext import commands
 # Permissions / JSON Parser
-from utils import permissions, default
+from utils import permissions, default, converters
 import typing
-
-class MemberID(commands.Converter):
-    async def convert(self, ctx, argument):
-        try:
-            m = await commands.MemberConverter().convert(ctx, argument)
-        except commands.BadArgument:
-            try:
-                return int(argument, base = 10)
-            except ValueError:
-                raise commands.BadArgument(f"Invalid member id.") from None
-        else:
-            return m.id
 
 
 class ModerationCog(commands.Cog, name = "Moderation"):
@@ -39,7 +27,7 @@ class ModerationCog(commands.Cog, name = "Moderation"):
     @commands.guild_only()
     @commands.has_permissions(ban_members = True)
     @commands.bot_has_permissions(ban_members = True)
-    async def ban(self, ctx, user: MemberID, time_limit: typing.Optional[int] = 7, *, reason: str = None):
+    async def ban(self, ctx, user: converters.MemberID, time_limit: typing.Optional[int] = 7, *, reason: str = None):
         """Bans an user from the server."""
 
         m = ctx.guild.get_member(user)
@@ -70,7 +58,6 @@ class ModerationCog(commands.Cog, name = "Moderation"):
         try:
             await ctx.guild.ban(discord.Object(id = user_id), reason = reason)
         except discord.NotFound:
-            await ctx.tick(tick = False)
             return await ctx.send(f"{self.emojis.cross} **I couldn't find an user with this ID.** `[ex NotFound]`")
         except Exception as e:
             return await ctx.send(f"{self.emojis.cross} **I couldn't ban this user.** `[ex {type(e).__name__}]`")
@@ -82,7 +69,7 @@ class ModerationCog(commands.Cog, name = "Moderation"):
     @commands.has_permissions(ban_members = True)
     @commands.bot_has_permissions(ban_members = True)
     async def softban(self, ctx, user: discord.User, time_limit: typing.Optional[int] = 7, *, reason: str = None):
-        """Bans an user and immediately them, clearing all their messages."""
+        """Bans an user and immediately unbans them, clearing all their messages."""
 
         m = ctx.guild.get_member(user.id)
         if m is not None and await permissions.verify_user_modcmd(ctx, m): return
@@ -119,14 +106,14 @@ class ModerationCog(commands.Cog, name = "Moderation"):
         else:
             await ctx.send(f"{self.emojis.tick} `{user} [{user.id}]` **has been unbanned. ({reason})**")
     
-    @commands.command(brief = 'user tools', aliases =['boot'], usage = '<@user/id>')
+    @commands.command(brief = 'user tools', aliases = ['boot'], usage = '<@user/id>')
     @commands.guild_only()
     @commands.has_permissions(kick_members = True)
     @commands.bot_has_permissions(kick_members = True)
     async def kick(self, ctx, member: discord.Member, *, reason: str = None):
         """Kicks a member from this server."""
 
-        if await permissions.check_modcmd_perms(ctx, member): return
+        if await permissions.verify_user_modcmd(ctx, member): return
         reason = reason or "No Reason Provided"
 
         try:
@@ -135,6 +122,47 @@ class ModerationCog(commands.Cog, name = "Moderation"):
             await ctx.send(f"{self.emojis.cross} **I couldn't kick this user.** `[ex {type(e).__name__}]`")
         else:
             await ctx.send(f"{self.emojis.tick} `{member} [{member.id}]` **has been kicked from the server. ({reason})**")
+    
+    @commands.command(brief = 'user tools', usage = '<@user/id> [reason]')
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles = True)
+    @commands.bot_has_permissions(manage_roles = True)  # Also check if bot has manage_channels / manage_roles
+    async def mute(self, ctx, member: discord.Member, *, reason: str = None):
+        """Mute someone, prevents them from talking in the server."""
+
+        if await permissions.verify_user_modcmd(ctx, member): return
+
+        mute_role = discord.utils.get(ctx.guild.roles, name = "Muted")
+
+        if not mute_role:
+            return await ctx.send(f"{self.emojis.cross} **A 'Muted' role doesn't already exist. Use `.role create chilledBotMutedRole` to create one.**")
+
+        reason = reason or "No Reason Provided"
+        await member.add_roles(mute_role)
+
+        await ctx.send(f"{self.emojis.tick} `{member} [{member.id}]` **has been muted. ({reason})**")
+    
+    @commands.command(brief = 'user tools', usage = '<@user/id> [reason]')
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles = True)
+    @commands.bot_has_permissions(manage_roles = True)
+    async def unmute(self, ctx, member: discord.Member, *, reason: str = None):
+        """Unmute someone, allows them to talk in the server."""
+
+        if await permissions.verify_user_modcmd(ctx, member): return  # Not sure if this is really necessary.
+
+        mute_role = discord.utils.get(ctx.guild.roles, name = "Muted")
+
+        if not mute_role:
+            return await ctx.send(f"{self.emojis.cross} **A 'Muted' role doesn't already exist. Use `.role create chilledBotMutedRole` to create one.**")
+
+        if not mute_role in member.roles:
+            return await ctx.send(f"{self.emojis.cross} **This user is not muted.**")
+        
+        reason = reason or "No Reason Provided"
+        await member.remove_roles(mute_role)
+
+        await ctx.send(f"{self.emojis.tick} `{member} [{member.id}]` **has been unmuted. ({reason})**")
     
     @commands.command(brief = 'server tools', aliases = ['echo', 'repeat'], usage = '<message>')
     @commands.guild_only()
@@ -187,6 +215,56 @@ class ModerationCog(commands.Cog, name = "Moderation"):
         
         await ctx.message.delete()
         await ctx.channel.purge(limit = count)
+
+    @commands.group(brief = 'server tools', name = 'role', usage = '<create/clone/remove> [params]')
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles = True)
+    @commands.bot_has_permissions(manage_roles = True)
+    async def role(self, ctx):
+        """Manage server roles."""
+
+        if not ctx.invoked_subcommand:
+            raise commands.BadArgument('missing subcommand')
+
+    # role add interactive
+    # asks questions, do you want this? do you want that?
+    @role.command(name = 'add', aliases = ['create', 'make'], usage = '"<name>" [hoisted (yes/no)] [allow mention (yes/no)] (hex color code) [permissions integer]')
+    async def role_add(self, ctx, name: str, hoisted: bool = False, allow_mentions: bool = False, color: discord.Color = None, permissions: int = 0):
+        """Create a new role."""
+
+        color = color or discord.Color.default()
+
+        if name == 'chilledBotMutedRole':
+
+
+        new_role = await ctx.guild.create_role(name = name, color = color, hoist = hoisted, mentionable = allow_mentions)
+
+        await ctx.send(f"{self.emojis.tick} **Successfully created role {new_role.mention}.")
+    
+    @role.command(name = 'clone', aliases = ['duplicate'], usage = '<role> [new name]')
+    async def role_clone(self, ctx, target_role: discord.Role, name: str = None):
+        """Clones a role."""
+
+        # to-do: check if you can clone the role beforehand.
+
+        name = name or target_role.name
+        permissions = target_role.permissions
+        color = target_role.color
+        hoist = target_role.hoist
+        mentionable = target_role.mentionable
+
+        new_role = await ctx.guild.create_role(name = name, color = color, hoist = hoist, mentionable = mentionable, permissions = permissions)
+
+        await ctx.send(f"{self.emojis.tick} **Successfully duplicated {target_role.mention} with {new_role.mention}")   
+    
+    @role.command(name = 'delete', aliases = ['remove'], usage = '<role> [reason]')
+    async def role_delete(self, ctx, target_role: discord.Role, *, reason: str = None):
+        """Deletes a role from the server."""
+
+        # to-do: check if you can delete the role beforehand
+        reason = reason or "No Reason Provided"
+        await target_role.delete(reason = reason)
+        await ctx.send(f"{self.emojis.tick} `@{target_role.name} [{target_role.id}]` **has been deleted. ({reason})**")
         
 def setup(bot):
     bot.add_cog(ModerationCog(bot))
