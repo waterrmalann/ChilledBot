f878da380a3782821d16db32f985e3b104c24f71
ALan <alan_biju@outlook.com>
2020-08-19 21:25:20 +0800

    improved quality of code

--- utils/default.py
+++ utils/default.py
@@ -1,11 +1,12 @@
 import json, collections
+from collections import namedtuple
 from humanize import naturaltime
 
-def get(file):
+def get(file: str):
     # Credits: AlexFlipnote | https://github.com/AlexFlipnote/discord_bot.py/blob/master/utils/default.py
     try:
         with open(file, encoding = 'utf8') as data:
-            return json.load(data, object_hook = lambda d: collections.namedtuple('X', d.keys())(*d.values()))
+            return json.load(data, object_hook = lambda d: namedtuple('X', d.keys())(*d.values()))
     except AttributeError:
         raise AttributeError("Unknown Argument")
     except FileNotFoundError:
@@ -15,6 +16,6 @@ def datefr(date, clock = True, humanize = True):
     to_return = date.strftime("%a, %b %d %y @ %I:%M %p") if clock else date.strftime("%A, %B %d %Y") # %S
     return f"{to_return} ({naturaltime(date)})" if humanize else to_return
 
-def int_suffix(i):
+def int_suffix(i: int) -> str:
     """Returns the ordinal representation of the given number."""
     return str(i) + {1: "st", 2: "nd", 3: "rd"}.get(i % 10 * (i % 100 not in {11, 12, 13}), "th")
