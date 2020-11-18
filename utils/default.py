33a9794ccad1d41baff77f464ac4a0af8b7bee56
ALan <alan_biju@outlook.com>
2020-11-18 13:44:30 +0800

    intent update

--- utils/default.py
+++ utils/default.py
@@ -19,3 +19,5 @@ def datefr(date, clock = True, humanize = True):
 def int_suffix(i: int) -> str:
     """Returns the ordinal representation of the given number."""
     return str(i) + {1: "st", 2: "nd", 3: "rd"}.get(i % 10 * (i % 100 not in {11, 12, 13}), "th")
+
+#RedditPost = namedtuple('RedditPost', 'author title text url nsfw comments upvotes link')
